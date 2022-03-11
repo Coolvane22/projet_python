e0a2089e0739ffb9359de531c1724d2dcc73c603
Evane Thivend <thivend.evane@gmail.com>
2022-03-11 10:06:50 +0100

    Listes des astronautes dans la station spatial einternationale

--- astronautes.py
+++ astronautes.py
@@ -1,5 +1,7 @@
 import requests
 
 r = requests.get('http://api.open-notify.org/astros.json', auth=('user', 'pass'))
+
 for p in r.json()["people"]:
-    print(p)
+    if p["craft"] == "ISS" :
+        print(p)
