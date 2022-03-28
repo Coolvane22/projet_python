909dca749979942fd2699e644bcf15371a4e19b0
Coolvane22 <99664579+Coolvane22@users.noreply.github.com>
2022-03-28 16:09:50 +0200

    Delete astronautes.py

--- astronautes.py
+++
@@ -1,9 +0,0 @@
-## Thivend Evane 
-
-import requests
-
-r = requests.get('http://api.open-notify.org/astros.json', auth=('user', 'pass'))
-
-for p in r.json()["people"]:
-    if p["craft"] == "ISS" :
-        print(p)
